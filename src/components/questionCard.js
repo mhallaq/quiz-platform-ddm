701d3597f774327e52d734db5370374f3d9c7d2c
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-11-17 20:38:17 -0500

    fixed useEffect error

--- src/components/questionCard.js
+++ src/components/questionCard.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { fetchRand } from '../services/apiConfig'
+import React from 'react';
 // import Container from '@material-ui/core/Container';
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
