5b1a16e2b92e668480a67eef10f303f0c6f984de
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-11-18 05:25:17 -0500

    resolved netlify issue

--- src/services/maxBet.js
+++ src/services/maxBet.js
@@ -1,17 +1,17 @@
 const establishMaxBet = (round, bank) => {
-    if (round == 1) {
+    if (round === 1) {
         if (bank <= 1000) {
             return 1000
         } else {
             return bank
         }
-    } if (round == 2) {
+    } if (round === 2) {
         if (bank <= 2000) {
             return 2000
         } else {
             return bank
         }
-    } if (round == 3) {
+    } if (round === 3) {
         if (bank <= 0) {
             return 0
         } else {
