fd32d069c2e21bdab35e15609dc420c9d7d41d17
david-diep <ddkotan@gmail.com>
2020-11-18 19:38:07 -0800

    namechange for boolean

--- src/components/jeopardyGrid.js
+++ src/components/jeopardyGrid.js
@@ -34,7 +34,7 @@ export default function JeopardyGrid(props) {
     <Grid container direction="row" className={classes.main}>
       {board &&
         board[round-1].map((column, index) => {
-          const taken = props.history[index];
+          const notTaken = props.history[index];
 
           return (
             <Grid key={index} md={2} container direction="column" item>
@@ -44,42 +44,42 @@ export default function JeopardyGrid(props) {
               <Box
                 className={`${classes.item} ${classes.question}`}
                 onClick={() => {
-                  if (taken[0]) itemClick(index, 0, 200 * round);
+                  if (notTaken[0]) itemClick(index, 0, 200 * round);
                 }}
               >
-                {taken[0] && <h2>${200*round}</h2>}
+                {notTaken[0] && <h2>${200*round}</h2>}
               </Box>
               <Box
                 className={`${classes.item} ${classes.question}`}
                 onClick={() => {
-                  if (taken[1]) itemClick(index, 1, 400 * round);
+                  if (notTaken[1]) itemClick(index, 1, 400 * round);
                 }}
               >
-                {taken[1] && <h2>${400 * round}</h2>}
+                {notTaken[1] && <h2>${400 * round}</h2>}
               </Box>
               <Box
                 className={`${classes.item} ${classes.question}`}
                 onClick={() => {
-                  if (taken[2]) itemClick(index, 2, 600 * round);
+                  if (notTaken[2]) itemClick(index, 2, 600 * round);
                 }}
               >
-                {taken[2] && <h2>${600 * round}</h2>}
+                {notTaken[2] && <h2>${600 * round}</h2>}
               </Box>
               <Box
                 className={`${classes.item} ${classes.question}`}
                 onClick={() => {
-                  if (taken[3]) itemClick(index, 3, 800 * round);
+                  if (notTaken[3]) itemClick(index, 3, 800 * round);
                 }}
               >
-                {taken[3] && <h2>${800 * round}</h2>}
+                {notTaken[3] && <h2>${800 * round}</h2>}
               </Box>
               <Box
                 className={`${classes.item} ${classes.question}`}
                 onClick={() => {
-                  if (taken[4]) itemClick(index, 4, 1000 * round);
+                  if (notTaken[4]) itemClick(index, 4, 1000 * round);
                 }}
               >
-                {taken[4] && <h2>${1000 * round}</h2>}
+                {notTaken[4] && <h2>${1000 * round}</h2>}
               </Box>
             </Grid>
           );
